afb802224d782b00267e73ace236e0e7cea6601d
Natanael Melo <109748470+Natanael135@users.noreply.github.com>
2025-06-02 14:09:52 -0300

    feat: implementando montagem de input

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,10 +1,19 @@
 import styles from "./ContactForm.module.css";
+import Input from "../Input/Input";
+import { FaUser } from "react-icons/fa";
 
 function ContactForm() {
   return (
     <section className={styles.contact} id="contato">
       <h2>Fale com a gente</h2>
       <form onSubmit={(e) => e.preventDefault()}>
+        <Input
+          label="Nome"
+          placeholder="Digite seu Nome"
+          icon={<FaUser />}
+          required
+        />
+
         <input type="text" name="nome" placeholder="Seu nome" required />
         <input type="email" name="email" placeholder="Seu e-mail" required />
         <textarea
